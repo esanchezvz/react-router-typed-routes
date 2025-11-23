a480250b1ec56bd3f6fe6ea6ffb9089422abbc43
Esteban Sánchez <esteban.sanvaz@gmail.com>
2025-11-23 03:19:22 -0600

    chore: improve jsDocs for util types

--- types/utilities.ts
+++ types/utilities.ts
@@ -2,8 +2,26 @@ declare global {
   namespace Utils {
     type DefaultRecursionLimit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+    /**
+     * Utiliy type to test if a string mathes a specified pattern.
+     *
+     * @example
+     * ```typescript
+     * type Match = MatchPattern<'/some-string/', `/${string}/`> // -> true
+     * type Match = MatchPattern<'some-string', `/${string}/`> // -> false
+     * ```
+     */
     type MatchPattern<S extends string, P> = S extends P ? true : false;
 
+    /**
+     * Utility type to create a tuple of strings by  splitting a string `S` by delimiter `D` into a tuple of strings.
+     *
+     * @example
+     * ```typescript
+     * type Parts = Split<'/users/some-id/edit', '/'>  // -> ["", "users", "some-id", "edit"]
+     * type Parts = Split<'dot.concatenated.string', '.'> //  -> ["dot", "concatenated", "string"]
+     * ```
+     */
     type Split<
       S extends string,
       D extends string
